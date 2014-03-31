8f283f4e3342aebb4cec3f313b58fd7347cc72b6
ConnorJ <cjenning@iastate.edu>
2014-03-30 21:01:48 -0500

    Searchable Dataframe

--- Shiny/UI and Server/server.R
+++ Shiny/UI and Server/server.R
@@ -14,10 +14,48 @@ shinyServer(function(input, output, session) {
     updateSelectInput(session, "Major", choices = Major, selected="All")
   })
   
+  observe({
+    input$goButton
+    df <- inputData
   
+  if (input$y12 == FALSE) {
+    df <- subset(df, Year != "2011-2012")
+  }
+  if (input$y11 == FALSE) {
+    df <- subset(df, Year != "2010-2011")
+  }
+  if (input$y10 == FALSE) {
+    df <- subset(df, Year != "2009-2010")
+  }
+  if (input$y09 == FALSE) {
+    df <- subset(df, Year != "2008-2009")
+  }
+  if (input$y08 == FALSE) {
+    df <- subset(df, Year != "2007-2008")
+  }
   
+  if (input$College == "All" & input$Major == "All"){df}
+  
+  if (input$College != "All" & input$Major == "All"){df <- subset(df, College == input$College)}
+  
+  if (input$College != "All" & input$Major != "All"){
+    df <- subset(df, College == input$College & Major == input$Major)
+  }
+  
+  df$Location <- paste(df$City, df$State)
+ # return(df)
 
-  output$people1 = renderTable({
+})
+  
+  
+  
+  
+  
+  
+  
+  ?renderDataTable
+
+  output$people1 = renderDataTable({
     df <- inputData
     
     if (input$y12 == FALSE) {
@@ -45,12 +83,13 @@ shinyServer(function(input, output, session) {
     }
     
     df$Location <- paste(df$City, df$State)
+  
     dfTable <- df[, c(6, 3, 2, 14, 7)]
     
     return(dfTable)
     #print(df)
 
- },include.rownames=FALSE)
+ })
   
 
   })
